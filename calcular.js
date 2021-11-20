48b0a8310d2ce59ec6ddb846c8500db3b9c1e73c
ETHEL <ethelvecchietti@gmail.com>
2021-11-20 18:15:26 -0300

    ah

--- calcular.js
+++ calcular.js
@@ -1,23 +1,18 @@
-var calc = document.getElementById('calculus')
+var contract = document.getElementById("contract");
+var mode = document.getElementById("mode");
+var precio_entrada = document.getElementById("precio_entrada");
+var precio_salida = document.getElementById("precio_salida");
+var margen_inicial = document.getElementById("margen_inicial");
+var balance = document.getElementById("wallet");
 
 
-
-// var contract = prompt("INGRESA TU PAR DE ALL COIN");
-// var mode = prompt("selecciona modo");
-// var precio_entrada = prompt("precio de entrada en usdt");
-// var precio_salida = prompt("precio de salida en usdt");
-// var margen_inicial = prompt("margin inicial en usdt");
-// var balance = prompt("wallet Balance en usdt")
 // var calcular; ///no se si va esta
 // var quanty_crip;
 // var pnl_usdt;
 // var roe_porcentual;
 // var liqui_price;
 
-quanty_crip = ///ecuacion
-    pnl_usdt = /// ecuacion 
-    roe_porcentual = /// ecuacion
-    liqui_price = /// ecuacion
-
-
-    document.write("tus resultados son: " + quanty_crip + "Crypto");
+//quanty_crip = ///ecuacion
+//pnl_usdt = /// ecuacion 
+//roe_porcentual = /// ecuacion
+//liqui_price = /// ecuacion
