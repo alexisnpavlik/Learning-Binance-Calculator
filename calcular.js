276d5269e688c15cfca4868b1bd2a39ed089b374
Alexis Medina <alexisnpavlik@gmail.com>
2021-11-19 21:48:48 -0300

    Ethel

---
+++ calcular.js
@@ -0,0 +1,22 @@
+var calc = document.getElementById('calculus')
+
+
+var contract = prompt("INGRESA TU PAR DE ALL COIN");
+var mode = prompt("selecciona modo");
+var precio_entrada = prompt("precio de entrada en usdt");
+var precio_salida = prompt("precio de salida en usdt");
+var margen_inicial = prompt("margin inicial en usdt");
+var balance = prompt("wallet Balance en usdt")
+var calcular; ///no se si va esta
+var quanty_crip;
+var pnl_usdt;
+var roe_porcentual;
+var liqui_price;
+
+quanty_crip = ///ecuacion
+    pnl_usdt = /// ecuacion 
+    roe_porcentual = /// ecuacion
+    liqui_price = /// ecuacion
+
+
+    document.write("tus resultados son: " + quanty_crip + "Crypto");
