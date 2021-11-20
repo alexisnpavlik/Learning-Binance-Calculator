18571d020eb37330e03af4a6b209e33e39df2093
Alexis Medina <alexisnpavlik@gmail.com>
2021-11-19 22:24:34 -0300

    update 2

--- calcular.js
+++ calcular.js
@@ -1,17 +1,18 @@
 var calc = document.getElementById('calculus')
 
 
-var contract = prompt("INGRESA TU PAR DE ALL COIN");
-var mode = prompt("selecciona modo");
-var precio_entrada = prompt("precio de entrada en usdt");
-var precio_salida = prompt("precio de salida en usdt");
-var margen_inicial = prompt("margin inicial en usdt");
-var balance = prompt("wallet Balance en usdt")
-var calcular; ///no se si va esta
-var quanty_crip;
-var pnl_usdt;
-var roe_porcentual;
-var liqui_price;
+
+// var contract = prompt("INGRESA TU PAR DE ALL COIN");
+// var mode = prompt("selecciona modo");
+// var precio_entrada = prompt("precio de entrada en usdt");
+// var precio_salida = prompt("precio de salida en usdt");
+// var margen_inicial = prompt("margin inicial en usdt");
+// var balance = prompt("wallet Balance en usdt")
+// var calcular; ///no se si va esta
+// var quanty_crip;
+// var pnl_usdt;
+// var roe_porcentual;
+// var liqui_price;
 
 quanty_crip = ///ecuacion
     pnl_usdt = /// ecuacion 
