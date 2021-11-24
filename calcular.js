b8793c4168a932f0518cf2765b31f70961ea40f6
ETHEL <ethelvecchietti@gmail.com>
2021-11-24 12:12:03 -0300

    mierc

--- calcular.js
+++ calcular.js
@@ -7,6 +7,9 @@ var getDataLong = function() {
     var calcularL;
     calcularL = precio_entradaL * 100;
     console.log(calcularL);
+    document.getElementById("resultado_long").style.visibility = 'visible';
+    document.getElementById("resultado_long").innerHTML = getDataLong();
+    resultado_long.innerHTML = '<div>su resultado es:' + getDatalong() + '</div>'
 };
 
 
@@ -20,20 +23,3 @@ var getDataShort = function() {
     calcularSh = precio_entradaSh * 100;
     console.log(calcularSh);
 };
-
-///document.getElementById('hola').style.visibility = 'visible';
-///document.getElementById('hola').innerHTML = getData();
-///hola.innerHTML = '<div>su resultado es:' + getData() + '</div>';
-
-
-
-// var calcular; ///no se si va esta
-// var quanty_crip;
-// var pnl_usdt;
-// var roe_porcentual;
-// var liqui_price;
-
-//quanty_crip = ///ecuacion
-//pnl_usdt = /// ecuacion 
-//roe_porcentual = /// ecuacion
-//liqui_price = /// ecuacion
