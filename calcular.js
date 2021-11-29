3c73696bac29591b6e55d83e8278480e23cdef31
Alexis Medina <alexisnpavlik@gmail.com>
2021-11-29 03:19:31 -0300

    .

--- calcular.js
+++ calcular.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var calc = document.getElementById('calculus')
 
 
@@ -21,3 +22,40 @@ quanty_crip = ///ecuacion
 
 
     document.write("tus resultados son: " + quanty_crip + "Crypto");
+=======
+const input = document.querySelector('input'); // Get the input element
+input.addEventListener('change', getDataLong); //Cuando el input cambie ejecute la función
+
+
+function getDataLong() {
+    var entradaL = document.getElementById("precio_entrada_long").value;
+    // var salidaL = document.getElementById("precio_salida_long").value;
+    // var apalancamientoL = document.getElementById("apalancamiento_long").value;
+    // var margen_inicialL = document.getElementById("margen_inicial_long").value;
+    // var balanceL = document.getElementById("balance_long").value;
+
+    const value = entradaL * 4; 
+
+    const log = document.getElementById('log');
+    log.innerHTML = value;
+
+}
+
+
+
+
+
+// var getDataShort = function() {
+//     var entradaSh = document.getElementById("precio_entrada_short").value;
+//     var salidaSh = document.getElementById("precio_salida_short").value;
+//     var apalancamientoSh = document.getElementById("apalancamiento_short").value;
+//     var margen_inicialSh = document.getElementById("margen_inicial_short").value;
+//     var balanceSh = document.getElementById("balance_short").value;
+//     var calcularSh;
+//     calcularSh = precio_entradaSh * 100;
+//     console.log(calcularSh);
+// };
+
+
+
+>>>>>>> 5afdfa1 (update)
