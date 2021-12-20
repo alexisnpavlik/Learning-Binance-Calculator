526c72c2978198588559b7fdbf95886b855ed13d
Alexis Medina <alexisnpavlik@gmail.com>
2021-12-20 14:24:41 -0300

    html funcion

--- calcular.js
+++ calcular.js
@@ -1,61 +1,29 @@
-<<<<<<< HEAD
-var calc = document.getElementById('calculus')
+// function getDataLong() {
+//     var entradaL = document.getElementById("precio_entrada_long").value;
+//     // var salidaL = document.getElementById("precio_salida_long").value;
+//     // var apalancamientoL = document.getElementById("apalancamiento_long").value;
+//     // var margen_inicialL = document.getElementById("margen_inicial_long").value;
+//     // var balanceL = document.getElementById("balance_long").value;
 
+//     const value = entradaL * 4; 
 
+//     const log = document.getElementById('log');
+//     log.innerHTML = value;
 
-// var contract = prompt("INGRESA TU PAR DE ALL COIN");
-// var mode = prompt("selecciona modo");
-// var precio_entrada = prompt("precio de entrada en usdt");
-// var precio_salida = prompt("precio de salida en usdt");
-// var margen_inicial = prompt("margin inicial en usdt");
-// var balance = prompt("wallet Balance en usdt")
-// var calcular; ///no se si va esta
-// var quanty_crip;
-// var pnl_usdt;
-// var roe_porcentual;
-// var liqui_price;
-
-quanty_crip = ///ecuacion
-    pnl_usdt = /// ecuacion 
-    roe_porcentual = /// ecuacion
-    liqui_price = /// ecuacion
-
-
-    document.write("tus resultados son: " + quanty_crip + "Crypto");
-=======
-const input = document.querySelector('input'); // Get the input element
-input.addEventListener('change', getDataLong); //Cuando el input cambie ejecute la función
+    
+// 
 
 
 function getDataLong() {
     var entradaL = document.getElementById("precio_entrada_long").value;
-    // var salidaL = document.getElementById("precio_salida_long").value;
+    var salidaL = document.getElementById("precio_salida_long").value;
+    var margen_inicialL = document.getElementById("margen_inicial_long").value;
     // var apalancamientoL = document.getElementById("apalancamiento_long").value;
-    // var margen_inicialL = document.getElementById("margen_inicial_long").value;
     // var balanceL = document.getElementById("balance_long").value;
 
-    const value = entradaL * 4; 
+    const value = salidaL * margen_inicialL / entradaL - margen_inicialL; 
 
     const log = document.getElementById('log');
     log.innerHTML = value;
 
 }
-
-
-
-
-
-// var getDataShort = function() {
-//     var entradaSh = document.getElementById("precio_entrada_short").value;
-//     var salidaSh = document.getElementById("precio_salida_short").value;
-//     var apalancamientoSh = document.getElementById("apalancamiento_short").value;
-//     var margen_inicialSh = document.getElementById("margen_inicial_short").value;
-//     var balanceSh = document.getElementById("balance_short").value;
-//     var calcularSh;
-//     calcularSh = precio_entradaSh * 100;
-//     console.log(calcularSh);
-// };
-
-
-
->>>>>>> 5afdfa1 (update)
