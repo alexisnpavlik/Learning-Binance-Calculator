8abf3f79549a9d39dcdffd00b62292a4f7702b9c
Alexis Medina <alexisnpavlik@gmail.com>
2021-12-20 17:56:53 -0300

    update

--- calcular.js
+++ calcular.js
@@ -6,21 +6,16 @@ function getDataLong() {
     var entradaL = document.getElementById("precio_entrada_long").value;
     var salidaL = document.getElementById("precio_salida_long").value;
     var margen_inicialL = document.getElementById("margen_inicial_long").value;
-    // var apalancamientoL = document.getElementById("apalancamiento_long").value;
-    // var balanceL = document.getElementById("balance_long").value;
-    const value = salidaL * margen_inicialL / entradaL - margen_inicialL;
-
+    var apalancamientoL = document.getElementById("apalancamineto_long").value;
+    var walletL = document.getElementById("waller_long").value;
+    
+    const value_PNL = salidaL * margen_inicialL * apalancamientoL / entradaL - margen_inicialL * apalancamientoL ;
     const log = document.getElementById('log');
-    log.innerHTML = value.toFixed(2);
+    log.innerHTML = value_PNL;
 }
+    
+//     const value_liq = (walletL+margen_inicialL+margen_inicialL*apalancamientoL*entradaL)/(apalancamientoL*margen_inicialL);
+//     const leg = document.getElementById('leg');
+//     leg.innerHTML = value_liq;
+// 
 
-function getDataShort() {
-    var entradaS = document.getElementById("precio_entrada_short").value;
-    var salidaS = document.getElementById("precio_salida_short").value;
-    var margen_incialS = document.getElementById("margen_inicial_short").value;
-
-    const value = salidaS * margen_incialS / entradaS - margen_incialS;
-
-    const log = document.getElementById('log');
-    log.innerHTML = value.toFixed(2);
-}
