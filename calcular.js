0fd27dd6fe3aa00edf9dad73a7124e4574a748a0
Alexis Medina <alexisnpavlik@gmail.com>
2021-12-21 01:17:34 -0300

    Update calcular.js

--- calcular.js
+++ calcular.js
@@ -21,9 +21,20 @@ function getDataLong() {
     // var apalancamientoL = document.getElementById("apalancamiento_long").value;
     // var balanceL = document.getElementById("balance_long").value;
 
+<<<<<<< Updated upstream
     const value = salidaL * margen_inicialL / entradaL - margen_inicialL; 
 
     const log = document.getElementById('log');
     log.innerHTML = value;
 
 }
+=======
+    const value_PNL = (salidaL * margen_inicialL * apalancamientoL / entradaL) - margen_inicialL * apalancamientoL;
+    const long = document.getElementById('long');
+    long.innerHTML = value_PNL;
+    
+    const value_liq = (entradaL * margen_inicialL) / (walletL + margen_inicialL);
+    const leg = document.getElementById('leg');
+    leg.innerHTML = value_liq;
+}
+>>>>>>> Stashed changes
